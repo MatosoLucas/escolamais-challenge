d370c8a6d5a2553208ecf8bf0573ef07df55be7d
Lucas Castro <lucas.castro@enext.com.br>
2021-10-13 18:03:45 -0300

    basic logic for adding a new TODO and changing TODOs status

--- src/components/TodosContainer.tsx
+++ src/components/TodosContainer.tsx
@@ -29,9 +29,17 @@ const TodoInfo = styled.div`
   margin: 25px;
 `
 
+const CreateTodo = styled.div`
+  input {
+    margin: 30px;
+    border: 1px solid #000;
+  }
+`
+
 export default function ToDosContainer() {
   let { id } = useParams<QuizParams>();
   const [todos, setTodos] = useState<Array<Todo>>([]);
+  const [addTitle, setTitle] = useState('')
 
   useEffect(() => {
     async function fetchData() {
@@ -41,14 +49,25 @@ export default function ToDosContainer() {
     fetchData()
   }, [id])
 
+  const PostTodo = (title: string) => {
+    fetch('https://jsonplaceholder.typicode.com/todos/', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: title,
+        completed: false,
+        userId: id,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        setTodos([...todos, json])
+      });
+  }
+
   const PostStatus = (todo) => {
-    todo.completed = !todo.completed
-    const newTodo = {
-      ...todo, completed: todo.completed
-    }
-    const index = todos.findIndex(el => el.id === todo.id)
-    todos[index] = newTodo;
-    console.log(todos)
     fetch(`https://jsonplaceholder.typicode.com/todos/${todo.id}`, {
       method: 'PATCH',
       body: JSON.stringify({
@@ -57,20 +76,31 @@ export default function ToDosContainer() {
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
       },
-    }).then((response) => response.json())
-      .then((json) => console.log(json));
+    }).then(() => {
+      const newTodo = {
+        ...todo, completed: !todo.completed
+      }
+      const index = todos.findIndex(el => el.id === todo.id)
+      const newTodos = [...todos];
+      newTodos[index] = newTodo;
+      setTodos(newTodos)
+    });
   }
 
   return (
     <TodoContainer>{todos.map(todo => (
-      <TodoInfo>
+      <TodoInfo key={todo.id}>
         <span>{todo.userId}</span>
         <span>{todo.id}</span>
         <span>{todo.title}</span>
         <span>{todo.completed ? 'Completed' : 'Pending'}</span>
         <button onClick={() => PostStatus(todo)}>Toggle Completion</button>
       </TodoInfo>
-    ))}</TodoContainer>
-
+    ))}
+      <CreateTodo>
+        <input placeholder="title" onChange={(e) => setTitle(e.target.value)}></input>
+        <button onClick={() => PostTodo(addTitle)}></button>
+      </CreateTodo>
+    </TodoContainer>
   )
 }
